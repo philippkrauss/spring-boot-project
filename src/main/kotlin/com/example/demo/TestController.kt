01598219e23f3bff6663226de729a79a47af0097
phikrau <philipp.p.krauss@mercedes-benz.com>
2024-03-21 11:40:37 +0100

    Add example for request parameter

--- src/main/kotlin/com/example/demo/TestController.kt
+++ src/main/kotlin/com/example/demo/TestController.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 data class TestDto(val test: String)
@@ -12,8 +13,10 @@ data class TestDto(val test: String)
 class TestController(private val testService: TestService) {
 
     @GetMapping("/test")
-    fun test(): TestDto {
+    fun test(
+        @RequestParam("prefix", required = false) prefix: String?,
+    ): TestDto {
         val test = testService.test()
-        return TestDto(test)
+        return TestDto(prefix.orEmpty() + test)
     }
 }
