37bd9570a4d728e783abe7577457c9425ccfe74e
phikrau <philipp.p.krauss@mercedes-benz.com>
2024-03-21 11:43:30 +0100

    Add test for request parameter

--- src/test/kotlin/com/example/demo/TestControllerTest.kt
+++ src/test/kotlin/com/example/demo/TestControllerTest.kt
@@ -1,9 +1,7 @@
 package com.example.demo
 
-import io.mockk.confirmVerified
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -22,6 +20,11 @@ class TestControllerTest(
     private val mockMvc: MockMvc,
     private val testService: TestService,
 ) {
+    @AfterEach
+    fun tearDown() {
+        clearAllMocks()
+    }
+
     @TestConfiguration
     class MockConfig {
         @Bean
@@ -35,6 +38,18 @@ class TestControllerTest(
         mockMvc.get("/test")
             .andExpect { status { isOk() } }
             .andExpect { content { json("{\"test\":  \"bsdf\"}", strict = true) } }
+
+        verify(exactly = 1) { testService.test() }
+        confirmVerified(testService)
+    }
+
+    @Test
+    fun `test test with prefix`() {
+        every { testService.test() } returns "bsdf"
+
+        mockMvc.get("/test?prefix=some-prefix_")
+            .andExpect { status { isOk() } }
+            .andExpect { content { json("{\"test\":  \"some-prefix_bsdf\"}", strict = true) } }
 
         verify(exactly = 1) { testService.test() }
         confirmVerified(testService)
